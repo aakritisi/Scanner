034c89801a42a2cf6889d952379cdf00e90d860c
aakriti <aakriti@172.16.102.46>
2020-10-06 07:14:07 -0400

    Implemented remaining methods.

--- assign1/src/cop5556fa20/SimpleParser.java
+++ assign1/src/cop5556fa20/SimpleParser.java
@@ -81,10 +81,9 @@ public class SimpleParser {
 			if(t.kind() == SEMI)
 			{
 				consume();
-			}else
-			{
-				throw new SyntaxException(t , "Syntax error");
 			}
+			else
+				throw new SyntaxException(t , "Syntax error");
 		}
 		
 	}
@@ -92,7 +91,19 @@ public class SimpleParser {
 
 	//make this public for convenience testing
 	public void expression() throws SyntaxException, LexicalException {
-		
+		orExpression();
+		if(t.kind() == Q)
+		{
+			consume();
+			expression();
+			if(t.kind() == COLON)
+			{
+				consume();
+				expression();
+			}
+			else
+				throw new SyntaxException(t , "Syntax error");
+		}
 	}
 	
 	public void declaration() throws SyntaxException, LexicalException {
@@ -175,84 +186,316 @@ public class SimpleParser {
 	}
 	
 	public void statement() throws SyntaxException, LexicalException {
-		//TODO
+	
+		if(t.kind() == IDENT)
+		{
+			consume();
+			if(t.kind() == RARROW )
+				imageOutStatement();
+			else if(t.kind() == LARROW)
+				imageInStatement();
+			else if(t.kind() == ASSIGN)
+				assingmentStatement();
+			else
+				throw new SyntaxException(t , "Syntax error");
+		}
+		else
+			throw new SyntaxException(t , "Syntax error");
 	}
 	
 	public void imageOutStatement() throws SyntaxException, LexicalException {
-		//TODO
+		if(t.kind() == RARROW)
+		{
+			consume();
+			if(t.kind() == KW_SCREEN)
+			{
+				consume();
+				if(t.kind() == LSQUARE)
+				{
+					consume();
+					expression();
+					if(t.kind() == COMMA)
+					{
+						consume();
+						expression();
+						if(t.kind() == RSQUARE)
+							consume();
+						else
+							throw new SyntaxException(t , "Syntax error");
+					}
+					else
+						throw new SyntaxException(t , "Syntax error");
+					
+				}
+			}
+			else
+				expression();
+		}
+		else
+			throw new SyntaxException(t , "Syntax error");
 	}
 	
 	public void imageInStatement() throws SyntaxException, LexicalException {
-		//TODO
+		if(t.kind() == LARROW)
+		{
+			consume();
+			expression();
+		}
+		else
+			throw new SyntaxException(t , "Syntax error");
+			
 	}
 	
 	public void assingmentStatement() throws SyntaxException, LexicalException {
-		//TODO
+		if(t.kind() == ASSIGN)
+		{
+			consume();
+			if(t.kind() == STAR)
+				loopStatement();
+			else 
+				expression();
+		}
+		else
+			throw new SyntaxException(t , "Syntax error");
 	}
 	
 	public void loopStatement() throws SyntaxException, LexicalException {
-		//TODO
+		if(t.kind() == STAR)
+		{
+			consume();
+			constXYSelector();
+			if(t.kind() == COLON)
+			{
+				consume();
+				if(t.kind() == PLUS || t.kind() == MINUS || t.kind() == EXCL || t.kind() == INTLIT || t.kind() == IDENT || t.kind() == LPAREN || t.kind() == STRINGLIT || t.kind() == KW_X || t.kind() == KW_Y || t.kind() == CONST || t.kind() == LPIXEL || t.kind() == AT)
+				{
+					expression();
+				}
+				if(t.kind() == COLON)
+				{
+					consume();
+					expression();
+				}
+				else
+					throw new SyntaxException(t , "Syntax error");
+			}
+			else
+				throw new SyntaxException(t , "Syntax error");
+				
+		
+		}
+		else
+			throw new SyntaxException(t , "Syntax error");
 	}
 	
 	public void andExpression() throws SyntaxException, LexicalException {
-		//TODO
+		eqExpression();
+		while(t.kind() == AND)
+		{
+			consume();
+			eqExpression();
+		}
 	}
 	
 	public void orExpression() throws SyntaxException, LexicalException {
-		//TODO
+		andExpression();
+		while(t.kind() == OR)
+		{
+			consume();
+			andExpression();
+		}
 	}
 	
 	public void eqExpression() throws SyntaxException, LexicalException {
-		//TODO
+		relExpression();
+		while(t.kind() == EQ || t.kind() == NEQ)
+		{
+			consume();
+			relExpression();
+		}
 	}
 	
 	public void relExpression() throws SyntaxException, LexicalException {
-		//TODO
+		addExpression();
+		while(t.kind() == LT || t.kind() == GT || t.kind() == LE || t.kind() == GE)
+		{
+			consume();
+			addExpression();
+		}
 	}
 	
 	public void addExpression() throws SyntaxException, LexicalException {
-		//TODO
+		multExpression();
+		while(t.kind() == PLUS || t.kind() == MINUS)
+		{
+			consume();
+			multExpression();
+		}
 	}
 	
 	public void multExpression() throws SyntaxException, LexicalException {
-		//TODO
+		unaryExpression();
+		while(t.kind() == STAR || t.kind() == DIV || t.kind() == MOD)
+		{
+			consume();
+			unaryExpression();
+		}
 	}
 	
 	public void unaryExpression() throws SyntaxException, LexicalException {
-		//TODO
+		if(t.kind() == PLUS || t.kind() == MINUS)
+		{
+			consume();
+			unaryExpression();
+		}
+		else
+			unaryExpressionNotPlusMinus();
 	}
 	
 	public void unaryExpressionNotPlusMinus() throws SyntaxException, LexicalException {
-		//TODO
+		if(t.kind() == EXCL)
+		{
+			consume();
+			unaryExpression();
+		}
+		else
+			hashExpression();
 	}
 	
 	public void hashExpression() throws SyntaxException, LexicalException {
-		//TODO
+		primary();
+		while(t.kind() == HASH)
+		{
+			consume();
+			attribute();
+		}
 	}
 	
 	public void primary() throws SyntaxException, LexicalException {
-		//TODO
+		if(t.kind() == INTLIT || t.kind() == IDENT || t.kind() == LPAREN || t.kind() == STRINGLIT || t.kind() == KW_X || t.kind() == KW_Y || t.kind() == CONST || t.kind() == LPIXEL || t.kind() == AT)
+		{
+			if(t.kind() == LPAREN)
+			{
+				consume();
+				expression();
+				if(t.kind() == RPAREN)
+					consume();
+				else
+					throw new SyntaxException(t , "Syntax error");
+			}
+			else if(t.kind() == LPIXEL)
+				pixelConstructor();
+			else if(t.kind() == AT)
+				argExpression();
+			consume();
+			if(t.kind() == LSQUARE)
+				pixelSelector();
+				
+		}
+		else
+			throw new SyntaxException(t , "Syntax error");
 	}
 	
 	public void pixelConstructor() throws SyntaxException, LexicalException {
-		//TODO
+		if(t.kind() == LPIXEL)
+		{
+			consume();
+			expression();
+			if(t.kind() == COMMA)
+			{
+				consume();
+				expression();
+				if(t.kind() == COMMA)
+				{
+					consume();
+					expression();
+					if(t.kind() == RPIXEL)
+						consume();
+					else
+						throw new SyntaxException(t , "Syntax error");
+				}
+				else
+					throw new SyntaxException(t , "Syntax error");
+			}
+			else
+				throw new SyntaxException(t , "Syntax error");
+			
+		}
+		else
+			throw new SyntaxException(t , "Syntax error");
 	}
 	
 	public void pixelSelector() throws SyntaxException, LexicalException {
-		//TODO
+		if(t.kind() == LSQUARE)
+		{
+			consume();
+			expression();
+			if(t.kind() == COMMA)
+			{
+				consume();
+				expression();
+				if(t.kind() == RSQUARE)
+					consume();
+				else
+					throw new SyntaxException(t , "Syntax error");
+			}
+			else
+				throw new SyntaxException(t , "Syntax error");
+		}
+		else
+			throw new SyntaxException(t , "Syntax error");
+		
 	}
 	
 	public void attribute() throws SyntaxException, LexicalException {
-		//TODO
+		if(t.kind() == KW_WIDTH || t.kind() == KW_HEIGHT|| t.kind() == KW_RED || t.kind() == KW_GREEN || t.kind() == KW_BLUE)
+			consume();
+		else
+			throw new SyntaxException(t , "Syntax error");
 	}
 	
 	public void argExpression() throws SyntaxException, LexicalException {
-		//TODO
+		if(t.kind() == AT)
+		{
+			consume();
+			primary();
+		}
+		else
+			throw new SyntaxException(t , "Syntax error");
 	}
 	
 	public void constXYSelector() throws SyntaxException, LexicalException {
-		//TODO
+		if(t.kind() == LSQUARE)
+		{
+			consume();
+			if(t.kind() == KW_X)
+			{
+				consume();
+				if(t.kind() == COMMA)
+				{
+					consume();
+					if(t.kind() == KW_Y)
+					{
+						consume();
+						if(t.kind() == RSQUARE)
+						{
+							consume();
+						}
+						else
+							throw new SyntaxException(t , "Syntax error");
+					}
+					else
+						throw new SyntaxException(t , "Syntax error");
+				}
+				else
+					throw new SyntaxException(t , "Syntax error");
+			}
+			else
+				throw new SyntaxException(t , "Syntax error");
+		}
+		else
+			throw new SyntaxException(t , "Syntax error");
 	}
 	
-   //TODO--everything else.  Have fun!!
+
 }
